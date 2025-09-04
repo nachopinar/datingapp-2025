5653c435680e852e120e23d0c24a67a129e94ddd
Ignacio <ignacio.carbajal@correo.ucu.edu.uy>
2025-09-04 16:06:19 -0300

    .

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 }
 );
 
+builder.Services.AddCors();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
