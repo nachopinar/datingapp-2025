2c7f1ba543556063543fb8a23fabad314bf91f35
Ignacio <ignacio.carbajal@correo.ucu.edu.uy>
2025-09-08 14:44:47 -0300

    .

---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css'
+})
+export class Nav {
+  protected creds : any = {}
+
+  login() {
+    console.log(this.creds)
+  }
+}
+
+
