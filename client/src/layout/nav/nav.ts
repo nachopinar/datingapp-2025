2df1b5d1042e84735ff5d39c57d68c6d370ec186
Ignacio <ignacio.carbajal@correo.ucu.edu.uy>
2025-09-09 12:59:54 -0300

    .

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
   imports: [FormsModule],
   templateUrl: './nav.html',
-  styleUrl: './nav.css'
+  styleUrl: './nav.css',
 })
 export class Nav {
-  protected creds : any = {}
+  protected accountService = inject(AccountService);
+  protected creds: any = {};
 
   login() {
-    console.log(this.creds)
+    this.accountService.login(this.creds).subscribe({
+      next: (result) => {
+        console.log(result);
+        this.creds = {};
+      },
+      error: (error) => alert(error.message),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 }
-
-
