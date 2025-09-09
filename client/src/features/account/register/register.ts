e5b24168b583584c8d6d727fdf9d1d6d1a5c628f
Ignacio <ignacio.carbajal@correo.ucu.edu.uy>
2025-09-09 15:11:33 -0300

    .

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RegisterCreds } from '../../../types/user';
+import { RegisterCreds, User } from '../../../types/user';
 
 @Component({
   selector: 'app-register',
@@ -9,6 +9,8 @@ import { RegisterCreds } from '../../../types/user';
   styleUrl: './register.css'
 })
 export class Register {
+  membersFromHome = input.required<User[]>();
+  cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
 
   register(){
@@ -16,6 +18,6 @@ export class Register {
   }
 
   cancel(){
-    console.log('cancelled!')
+    this.cancelRegister.emit(false);
   }
 }
