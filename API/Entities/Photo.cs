74baf1ee4c22bf5229d5555f9fb95c9c6e41616b
Ignacio <ignacio.carbajal@correo.ucu.edu.uy>
2025-09-24 15:43:57 -0300

    .

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Globalization;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+
+    public required string Url { get; set; }
+
+    public string? PublicId { get; set; }
+
+
+    //Navigation property
+
+    public Member Member { get; set; } = null!;
+}
